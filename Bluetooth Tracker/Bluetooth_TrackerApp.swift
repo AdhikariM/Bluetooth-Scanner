316fbe35fa8933280d996ce20178b5b3acd296a4
AdhikariM <maheshadhikari@live.com>
2024-08-08 22:18:18 -0500

    Initial Commit

---
+++ Bluetooth Tracker/Bluetooth_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bluetooth_TrackerApp.swift
+//  Bluetooth Tracker
+//
+//  Created by Mahesh Adhikari on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Bluetooth_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

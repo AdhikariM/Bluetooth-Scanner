d1ab2923ba1232e554d102e5f229b125767cfeef
AdhikariM <maheshadhikari@live.com>
2024-11-09 03:19:28 -0600

    vm update for location?

--- Bluetooth Tracker/BluetoothViewModel.swift
+++ Bluetooth Tracker/BluetoothViewModel.swift
@@ -7,23 +7,30 @@
 
 import Foundation
 import CoreBluetooth
+import CoreLocation
 import Combine
+import MapKit
 
 struct BluetoothDevice: Identifiable {
     let id: UUID
     let name: String
     let rssi: NSNumber
     let count: Int
+    let coordinate: CLLocationCoordinate2D
 }
 
-class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate {
+class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate, CLLocationManagerDelegate {
     @Published var deviceMap = [UUID: BluetoothDevice]()
     @Published var searchText = ""
     @Published var selectedRSSIFilter = RSSIFilter.all
     @Published private(set) var filteredDevices: [BluetoothDevice] = []
     @Published private(set) var totalFilteredDevices: Int = 0
+    @Published var deviceLocations: [BluetoothDevice] = []
+    @Published var region: MKCoordinateRegion
+    @Published var userLocation: CLLocationCoordinate2D?
 
     private var centralManager: CBCentralManager?
+    private var locationManager: CLLocationManager?
 
     enum RSSIFilter: String, CaseIterable, Identifiable {
         case all = "RSSI: All"
@@ -35,9 +42,30 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate {
     }
 
     override init() {
+        self.region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194), // Default to San Francisco
+            span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        )
         super.init()
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.startUpdatingLocation()
+        
         centralManager = CBCentralManager(delegate: self, queue: nil)
-        updateFilteredDevices()
+    }
+
+    // CLLocationManagerDelegate method
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            userLocation = location.coordinate
+            updateRegion()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get user location: \(error.localizedDescription)")
     }
 
     func startScan() {
@@ -50,11 +78,13 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate {
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         let deviceId = peripheral.identifier
         let deviceName = peripheral.name ?? deviceId.uuidString
-        
+
+        let deviceLocation = CLLocationCoordinate2D(latitude: 90.00, longitude: 90.00)
+
         if let existingDevice = deviceMap[deviceId] {
-            deviceMap[deviceId] = BluetoothDevice(id: deviceId, name: existingDevice.name, rssi: RSSI, count: existingDevice.count + 1)
+            deviceMap[deviceId] = BluetoothDevice(id: deviceId, name: existingDevice.name, rssi: RSSI, count: existingDevice.count + 1, coordinate: deviceLocation)
         } else {
-            deviceMap[deviceId] = BluetoothDevice(id: deviceId, name: deviceName, rssi: RSSI, count: 1)
+            deviceMap[deviceId] = BluetoothDevice(id: deviceId, name: deviceName, rssi: RSSI, count: 1, coordinate: deviceLocation)
         }
 
         updateFilteredDevices()
@@ -83,5 +113,34 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate {
         }
         
         totalFilteredDevices = filteredDevices.count
+        deviceLocations = filteredDevices
+    }
+
+    func updateRegion() {
+        guard let userLocation = userLocation else { return }
+        
+        let latitudes = deviceLocations.map { $0.coordinate.latitude }
+        let longitudes = deviceLocations.map { $0.coordinate.longitude }
+        
+        let maxLat = latitudes.max() ?? 37.7749
+        let minLat = latitudes.min() ?? 37.7749
+        let maxLong = longitudes.max() ?? -122.4194
+        let minLong = longitudes.min() ?? -122.4194
+        
+        let span = MKCoordinateSpan(
+            latitudeDelta: (maxLat - minLat) * 1.5,
+            longitudeDelta: (maxLong - minLong) * 1.5
+        )
+        
+        let center = CLLocationCoordinate2D(
+            latitude: (maxLat + minLat) / 2,
+            longitude: (maxLong + minLong) / 2
+        )
+        
+        region = MKCoordinateRegion(center: center, span: span)
+    }
+
+    func isDeviceWithinRange(device: BluetoothDevice) -> Bool {
+        return device.rssi.intValue > -50
     }
 }
